332af5332fd5490503dd977119a067d7adad45ef
alexdelorenzo <you@example.com>
2020-10-03 00:49:51 -0400

    Extract from onhold

---
+++ ding/after.py
@@ -0,0 +1,33 @@
+from typing import Optional
+from pathlib import Path
+
+from onhold.base import dumb_pipe, using_path, is_pipeline, \
+  RC_ENV_VAR, RC_OK
+
+from play_sounds import play_after, DEFAULT_SOUND
+import click
+
+
+ENV_VAR = 'DING'
+
+
+def run(file: Path):
+  with play_after(file):
+    if not is_pipeline():
+      return
+
+    dumb_pipe()
+
+
+@click.command(help="Play specified sound after job is complete.")
+@click.option('-s', '--sound_path', required=False,
+  type=click.Path(exists=True), help="Path to sound to play.")
+@click.option('-w', '--warn', required=False,
+  is_flag=True, default=False, help="Show warnings.")
+def cmd(sound_path: Optional[str], warn: bool):
+  with using_path(sound_path, warn, DEFAULT_SOUND, ENV_VAR) as path:
+    run(path)
+
+
+if __name__ == "__main__":
+  cmd()
