cc2bc87153beb7f5cdcf1fb2765dff0628116157
alexdelorenzo <you@example.com>
2020-12-02 21:32:57 -0500

    Add terminal beep feature

---
+++ ding/__init__.py
@@ -0,0 +1,1 @@
+from .after import *
--- ding/after.py
+++ ding/after.py
@@ -2,14 +2,14 @@ from typing import Optional
 from pathlib import Path
 
 from onhold.base import dumb_pipe, using_path, is_pipeline
-from play_sounds import play_after, DEFAULT_SOUND
+from play_sounds import play_after, bell_after, DEFAULT_SOUND
 import click
 
 
 ENV_VAR = 'DING'
 
 
-def run(file: Path):
+def run(file: Optional[Path]):
   with play_after(file):
     if not is_pipeline():
       return
@@ -19,13 +19,15 @@ def run(file: Path):
 
 @click.command(help="Play specified sound after job is complete.")
 @click.option('-s', '--sound_path', required=False,
-  type=click.Path(exists=True),
-  help="Path to sound to play.")
+  type=click.Path(exists=True), help="Path to sound to play.")
+@click.option('-b', '--bell', required=False,
+  default=False, show_default=True, is_flag=True,
+  type=click.BOOL, help="Ring the terminal bell, as well.")
 @click.option('-w', '--warn', required=False,
-  is_flag=True, default=False,
-  help="Show warnings.")
-def cmd(sound_path: Optional[str], warn: bool):
-  with using_path(sound_path, warn, DEFAULT_SOUND, ENV_VAR) as path:
+  is_flag=True, default=False, help="Show warnings.")
+def cmd(sound_path: Optional[str], bell: bool, warn: bool):
+  with bell_after(bell), \
+       using_path(sound_path, warn, DEFAULT_SOUND, ENV_VAR) as path:
     run(path)
 
 
